35d61f031515bd7c5ccc9c983cb6e75adcb6486e
João Augusto Bonfante <joao.dev93@gmail.com>
2026-01-21 12:30:33 -0300

    add: refactor DateChallenge class constructor, implement Dinner, Food, Person, and NullValue classes with examples of object interactions and null handling

--- src/classes/challenges/DateChallenge.java
+++ src/classes/challenges/DateChallenge.java
@@ -7,30 +7,33 @@ public class DateChallenge {
     public int year;
 
     public DateChallenge() {
-        day = 1;
-        month = 1;
-        year = 1970;
+        //day = 1;
+        //month = 1;
+        //year = 1970;
+        this(1, 1, 1970);
     }
 
-    public DateChallenge(int initialDay, int initialMonth, int initialYear) {
-        day = initialDay;
-        month = initialMonth;
-        year = initialYear;
+    public DateChallenge(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 
     public String formatDateEN() {
-        return String.format("%d-%d-%d\n", year, month, day);
+        final String formatEN = "%d-%d-%d";
+        return String.format(formatEN, year, month, day);
     }
 
     public String formatDateBR() {
-        return String.format("%d/%d/%d", day, month, year);
+        final String formatBR = "%d/%d/%d";
+        return String.format(formatBR, day, month, year);
     }
 
     public void printDateEN() {
-        System.out.println(formatDateEN());
+        System.out.println(this.formatDateEN());
     }
 
     public void printDateBR() {
-        System.out.println(formatDateBR());
+        System.out.println(this.formatDateBR());
     }
 }
