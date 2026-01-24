5c742510105aeffaaffa71d0a5d660b5a7a36c06
João Augusto Bonfante <joao.dev93@gmail.com>
2026-01-24 18:32:52 -0300

    add: implement basic exception handling examples and validation logic; include custom exceptions for empty strings and out-of-range values

---
+++ src/exceptions/custom/two/EmptyStringException.java
@@ -0,0 +1,16 @@
+package exceptions.custom.two;
+
+@SuppressWarnings("serial")
+public class EmptyStringException extends Exception {
+
+    private String attributeName;
+
+    public EmptyStringException(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    @Override
+    public String getMessage() {
+        return String.format("The attribute %s is empty", attributeName);
+    }
+}
